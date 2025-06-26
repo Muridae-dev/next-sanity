b1fd1be9c532a5385a46990d064995a561fe4ecb
Muridae-dev <92300249+Muridae-dev@users.noreply.github.com>
2025-06-27 00:11:40 +0200

    okay this is actually kind of riddiculus how much WIP I'm doing so I'm gonna stop

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,10 @@
 import HomepageHero from "@/components/HomepageHero";
-import Image from "next/image";
 
 export default async function Page() {
   return (
     <>
       <HomepageHero />
-      <div className="h-content w-full border-t border-b flex items-center">
+      <div className="h-content w-full border-t border-b flex items-center gradient-background">
         <h2 className="text-8xl w-fit h-fit pl-[12px] pr-[24px] font-faodu">
           SUPPORTING PRIDE
         </h2>
@@ -17,20 +16,20 @@ export default async function Page() {
           />
         </div>
       </div>
-      <div className="grid grid-cols-3 border-b">
-        <div className="p-[12px] border-r">
+      <div className="grid grid-cols-3 border-b gradient-background">
+        <div className="p-[12px] border-r relative overflow-hidden">
           <h3 className="text-5xl pb-[12px] font-faodu">Raise your flag!</h3>
           Show your support by hanging a rainbow flag where you live.
         </div>
 
-        <div className="p-[12px] border-r">
+        <div className="p-[12px] border-r ">
           <h3 className="text-5xl pb-[12px] font-faodu">Buy our Merch</h3>
           We sell caps, shopping nets, stickers and flags. All the money goes
           directly to Henningsvær Pride. You can buy our merch at MIN
           Henningsvær or secondhand and div in Henningsvær.
         </div>
 
-        <div className="p-[12px]">
+        <div className="p-[12px] ">
           <h3 className="text-5xl pb-[12px] font-faodu">
             Follow us on Instagram
           </h3>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="z-10 fixed top-[0] left-[0] w-full flex justify-between">
+    <header className="z-10 fixed top-[0] left-[0] w-full flex justify-between font-faodu text-lg">
       <Link
         className="bg-white border-b border-r px-[20px] py-[12px] h-fit"
         href="/"
