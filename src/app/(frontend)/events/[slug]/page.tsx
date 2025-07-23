c08bac1262526ac71fbe22d2a19cbf753d19dad9
Muridae-dev <92300249+Muridae-dev@users.noreply.github.com>
2025-07-23 22:58:30 +0200

    fix errors, add higher res images & change header buttons

--- src/app/(frontend)/events/[slug]/page.tsx
+++ src/app/(frontend)/events/[slug]/page.tsx
@@ -21,38 +21,38 @@ export default async function Page({
     notFound();
   }
 
-  console.log(event);
-
   return (
     <main className="relative">
       <div className="h-screen flex flex-col items-center justify-center">
-        <h1 className="text-8xl w-fit h-fit pl-[12px] pr-[24px] font-faodu pb-[24px] text-center uppercase">
+        <h1 className="text-8xl w-fit h-fit pl-[12px] pr-[24px] font-faodu pb-[24px] text-center">
           {event.title}
         </h1>
         <div className="w-[80%] h-[400px] overflow-hidden flex items-center justify-center relative rounded">
-          <Image
-            className="absolute w-full"
-            width={1400}
-            height={800}
-            alt={event.mainImage?.alt ? event.mainImage?.alt : ""}
-            src={
-              event.mainImage
-                ? urlFor(event.mainImage).width(800).height(800).url()
-                : ""
-            }
-          />
+          {event.mainImage && (
+            <Image
+              src={urlFor(event.mainImage)
+                .width(1600)
+                .height(800)
+                .quality(100)
+                .auto("format")
+                .url()}
+              alt={event.mainImage?.alt || "Event image"}
+              width={1400}
+              height={800}
+            />
+          )}
         </div>
         <div className="pt-[24px]">
           {event.body ? (
-            <div className="lg:col-span-7 px-[8px] lg:col-start-6 prose text-center text-black pb-[12px]">
+            <div className="lg:col-span-7 px-[8px] lg:col-start-6 prose text-center text-inherit pb-[12px]">
               <PortableText value={event.body} components={components} />
             </div>
           ) : null}
         </div>
 
         <div>
-          <div className="uppercase font-bold">
-            {dayjs(event.eventDate).format("D MMMM YYYY")} ❤️ LOCATION
+          <div className="font-bold">
+            {dayjs(event.eventDate).format("D MMMM YYYY")} ❤️ Location
           </div>
         </div>
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,15 @@ export function Header() {
   return (
     <header className="z-10 fixed top-[0] left-[0] w-full flex justify-between font-faodu text-lg">
       <Link
-        className="bg-white border-b border-r px-[20px] py-[12px] h-fit hover:bg-gray-200"
+        className="menu-button border-b border-r px-[20px] py-[12px] h-fit"
         href="/"
       >
         Home
       </Link>
-      <ul className="flex items-center border-b border-l bg-white">
+      <ul className="flex items-center border-b border-l">
         <li>
           <Link
-            className="h-full flex items-center px-[32px] border-r py-[32px] hover:bg-gray-200"
+            className="menu-button h-full flex items-center px-[32px] border-r py-[32px]"
             href="/posts"
           >
             Program
@@ -20,7 +20,7 @@ export function Header() {
         </li>
         <li>
           <Link
-            className="h-full flex items-center px-[32px] py-[32px] hover:bg-gray-200"
+            className="menu-button h-full flex items-center px-[32px] py-[32px]"
             href="/studio"
           >
             Shop
