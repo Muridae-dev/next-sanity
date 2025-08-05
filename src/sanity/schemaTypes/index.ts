45c1566e32318a44eaafb0cdfd11d5690d7485e7
Muridae-dev <92300249+Muridae-dev@users.noreply.github.com>
2025-08-05 12:35:46 +0200

    add photo gallery in sanity & on front-page & image modal

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,7 +3,8 @@ import { type SchemaTypeDefinition } from "sanity";
 import { blockContentType } from "./blockContentType";
 import { categoryType } from "./categoryType";
 import { eventType } from "./eventType";
+import { photoType } from "./photoType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, eventType],
+  types: [blockContentType, categoryType, eventType, photoType],
 };
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -7,8 +7,11 @@ export const structure: StructureResolver = (S) =>
     .items([
       S.documentTypeListItem("event").title("Events"),
       S.documentTypeListItem("category").title("Categories"),
+      S.documentTypeListItem("photo").title("Photo Gallery"),
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !["event", "category"].includes(item.getId()!)
+        (item) =>
+          item.getId() &&
+          !["event", "category", "photo"].includes(item.getId()!)
       ),
     ]);
